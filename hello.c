0422659d1af5a8497a30617f6e7953d8c8d2173a
Your Name <youremail@huskers.unl.edu>
2021-08-31 02:15:25 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -12,6 +12,8 @@ int main(int argc, char **argv) {
 
     printf("Sonya Wu!\n");
     printf("Major: Mathematics\n");
+    printf("Haonan Sungit!\n");
+    printf("Major: Mathematics\n");
 
     return 0;
 }
